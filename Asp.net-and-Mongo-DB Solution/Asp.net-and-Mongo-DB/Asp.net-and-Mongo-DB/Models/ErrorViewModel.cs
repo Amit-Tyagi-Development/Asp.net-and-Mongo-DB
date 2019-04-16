9c54a06e9d49cd09e0c402c6cc01585a5d040923
AmitTyagi <36616520+AmitTyagi@users.noreply.github.com>
2019-04-16 10:45:07 +0100

    First Check-in

---
+++ Asp.net-and-Mongo-DB Solution/Asp.net-and-Mongo-DB/Asp.net-and-Mongo-DB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Asp.net_and_Mongo_DB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
